4e3140c2f2bf163e1536526e93526d8105ea920d
詹政勳 <nintendof1@gmail.com>
2019-06-30 10:47:42 +0800

    first commit

---
+++ socket_io/server.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+//   res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    // console.log('a user connected');
+    socket.on('chat message',(sendMseeage) => {
+        // console.log('message: ', sendMseeage);
+        io.emit('chat message',sendMseeage);
+    });
+    socket.on('disconnect', () => {
+        console.log('Bye~');  // 顯示 bye~
+    });
+});
+  
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
