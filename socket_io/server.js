97ffbb4dd202123145df04afadb691923b14b512
johnny <nintendof1@gmail.com>
2019-06-30 10:52:49 +0800

    'server_v1'

--- socket_io/server.js
+++ socket_io/server.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res){
 //   res.send('<h1>Hello world</h1>');
     res.sendFile(__dirname + '/index.html');
 });
-
+// 設定連線 處理(Connection + socket.on + io.emit)
 io.on('connection', function(socket){
     // console.log('a user connected');
     socket.on('chat message',(sendMseeage) => {
