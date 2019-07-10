6b18aac1bb36f9011be09e8508beaf612927138d
capeta0507 <nintendof1@gmail.com>
2019-07-10 21:59:44 +0800

    '加上註解'

--- chat_server_client/server.js
+++ chat_server_client/server.js
@@ -4,12 +4,11 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
 
-// Constants
-const PORT = 3000;
+// PORT
+const PORT = PORT || 3000;
 // const HOST = '127.0.0.1';
 
 // 提供靜態檔案顯示
-// console.log(__dirname + '/public');
 app.use('/' , express.static(__dirname + '/public'));
 
 // MongoDB Client + dbo + ObjectId (變數設定)
@@ -24,11 +23,6 @@ MongoClient.connect(url, { useNewUrlParser: true },function(err, db) {
     console.log('MongoDB 連線 chat-room...成功');
 });
 
-// app.get('/', function(req, res){
-// //   res.send('<h1>Hello world</h1>');
-//     res.sendFile(__dirname + '/index.html');
-// });
-
 // 取得所有 chat-room/real-time-chat 的所有資料
 app.get('/chatdata',(req,res) =>{
     // console.log('/chatdata ... get chat room data');
@@ -39,22 +33,25 @@ app.get('/chatdata',(req,res) =>{
 })
 // 設定連線 處理(Connection + socket.on + io.emit)
 io.on('connection', function(socket){
+    // Client連線
     console.log('a user connected');
     socket.on('chat message',(sendMessage) => {
         // console.log('收到 : ', sendMessage);
         sendMessage.timestamp = moment().valueOf();  //在 Server端 加入時間撮記
         io.emit('chat message',sendMessage);
-
         // 寫入 mongodb (mLab)(database:chat-room)(collection:chat-room)
         // 準備新的訊息
-        let newvalues = {   name: sendMessage.getName, 
-                            message: sendMessage.getMsg,
-                            timestamp:sendMessage.timestamp };
+        let newvalues = {
+            name: sendMessage.getName, 
+            message: sendMessage.getMsg,
+            timestamp:sendMessage.timestamp
+        };
         dbo.collection("real-time-chat").insertOne(newvalues, function(err, result){
             console.log('寫入 MongoDB ...' + sendMessage.getName + ":" + sendMessage.getMsg)
             // console.log(result.ops);
         });
-});
+    });
+    // Client離開
     socket.on('disconnect', () => {
         console.log('Bye~');  // 顯示 bye~
     });
